2eea403c60d0cc655c7e9d966220537e31ebbf11
MRxShoody <jacqueslin@live.fr>
2023-06-07 17:48:41 +0200

    commit

--- popup/popup.js
+++ popup/popup.js
@@ -2,9 +2,9 @@ const sendMessageId = document.getElementById("sendmessageid");
 const textpopup = document.getElementById("text");
 
 if (sendMessageId) {
-    sendMessageId.onclick = function () {
+    sendMessageId.onclick = function() {
         const message = textpopup.value; // get the text from the textarea
-        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             chrome.tabs.sendMessage(
                 tabs[0].id,
                 {
